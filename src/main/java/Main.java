940110698bb3ea8f89fd63cc6b26215e6d702c50
spMakumazan <sp.makumazan@gmail.com>
2023-04-18 22:02:04 +0300

    freqlog

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,49 +5,59 @@ public class Main {
     public static final Map<Integer, Integer> sizeToFreq = new HashMap<>();
 
     public static void main(String[] args) throws InterruptedException {
+        Thread printedThread = new Thread(() -> {
+            synchronized (sizeToFreq) {
+                while (!Thread.interrupted()) {
+                    try {
+                        sizeToFreq.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    int countRepetitionMaxValue = 0;
+                    for (Map.Entry<Integer, Integer> entry : sizeToFreq.entrySet()) {
+                        if (entry.getValue() > countRepetitionMaxValue) {
+                            countRepetitionMaxValue = entry.getValue();
+                        }
+                    }
+                    for (Map.Entry<Integer, Integer> entry : sizeToFreq.entrySet()) {
+                        if (entry.getValue() == countRepetitionMaxValue) {
+                            System.out.printf("Самое частое количество повторений %d (встретилось %d раз)\n",
+                                    entry.getKey(), countRepetitionMaxValue);
+                        }
+                    }
+                }
+            }
+        });
+        printedThread.start();
+
         List<Thread> threads = new ArrayList<>();
         for (int i = 0; i < 1000; i++) {
             Thread thread = new Thread(() -> {
-                String route = generateRoute("RLRFR", 100);
-                int count = 0;
-                for (int c = 0; c < route.length(); c++) {
-                    if (route.charAt(c) == 'R') {
-                        count++;
-                    }
-                }
                 synchronized (sizeToFreq) {
+                    String route = generateRoute("RLRFR", 100);
+                    int count = 0;
+                    for (int c = 0; c < route.length(); c++) {
+                        if (route.charAt(c) == 'R') {
+                            count++;
+                        }
+                    }
                     if (sizeToFreq.containsKey(count)) {
                         sizeToFreq.put(count, sizeToFreq.get(count) + 1);
                     } else {
                         sizeToFreq.put(count, 1);
                     }
+                    sizeToFreq.notify();
                 }
             });
             thread.start();
             threads.add(thread);
         }
+
+
         for (Thread thread : threads) {
             thread.join();
         }
-
-        int countRepetitionMaxValue = 0;
-        for (Map.Entry<Integer, Integer> entry : sizeToFreq.entrySet()) {
-            if (entry.getValue() > countRepetitionMaxValue) {
-                countRepetitionMaxValue = entry.getValue();
-            }
-        }
-        for (Map.Entry<Integer, Integer> entry : sizeToFreq.entrySet()) {
-            if (entry.getValue() == countRepetitionMaxValue) {
-                System.out.printf("Самое частое количество повторений %d (встретилось %d раз)\n",
-                        entry.getKey(), countRepetitionMaxValue);
-            }
-        }
-        System.out.println("Другие размеры:");
-        for (Map.Entry<Integer, Integer> entry : sizeToFreq.entrySet()) {
-            if (entry.getValue() != countRepetitionMaxValue) {
-                System.out.printf("- %d (%d раз)\n", entry.getKey(), entry.getValue());
-            }
-        }
+        printedThread.interrupt();
     }
 
     public static String generateRoute(String letters, int length) {
